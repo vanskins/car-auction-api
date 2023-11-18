2d8e6914dbc70baedfe03b501fdf39fec7d9fae8
vanalfredd <vansabacajan@gmail.com>
2023-11-18 15:51:18 +0800

    Update: Middleware added to route

---
+++ src/controllers/users.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { getUsers } from "../models/users";
+
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await getUsers();
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.sendStatus(400);
+  }
+}
