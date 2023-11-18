e2fba92567323777b34106c16846cff97879f4d4
vanalfredd <vansabacajan@gmail.com>
2023-11-18 09:59:29 +0800

    Update: Updated user model

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express'
+
+export const register = async (req: Request, res: Response ) => {
+  try {
+    
+  } catch (error) {
+    return res.sendStatus(400)
+  }
+}
--- src/models/users.ts
+++ src/models/users.ts
@@ -31,4 +31,28 @@ const UserSchema = new Schema({
 
 const User = models.User || model("User", UserSchema)
 
+type UserTypes = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  roles: string[]
+}
+
+// GET all users
+export const getUsers = () => User.find().lean();
+// GET user by id
+export const getUserById = (_id: string) => User.findById(_id).lean();
+// GET user by email
+export const getUserBySessionToken = (sessionToken: string) => User.findOne({
+  'authentication.sessionToken': sessionToken
+}).lean();
+export const getUserByEmail = (email: string) => User.findOne({ email }).lean();
+// Delete user by id
+export const deleteUserById = (_id: string) => User.findByIdAndDelete(_id);
+// Create new user
+export const createNewUSer = (values: Record<string, any>) => new User(values).save().then((user: any) => user.toObject())
+// Update user
+export const updateUserById = (_id: string, values: Record<string, any>) => User.findByIdAndUpdate(_id, values);
+
 export default User;
