d7e7d7b3712fa189c4d45de7a04149dc691fd769
vanalfredd <vansabacajan@gmail.com>
2023-11-18 15:39:47 +0800

    Added: Middleware added

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,8 +1,25 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { get, merge } from 'lodash';
 
 import { getUserBySessionToken } from '../models/users';
 
-export const isAuthenticated = async (req: Request, res: Response) => {
-  
+export const isAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const sessionToken = req.cookies['CAR-AUCTION-API-AUTH'];
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+
+    const checkExistingUser = await getUserBySessionToken(sessionToken)
+
+    if (!checkExistingUser) {
+      return res.sendStatus(403)
+    }
+
+    merge(req, { identity: checkExistingUser})
+
+    return next();
+  } catch (error) {
+    return res.sendStatus(401);
+  }
 }
